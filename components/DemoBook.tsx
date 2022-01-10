6fb5f2f02c3f3525db325cc0e9376d31039984a5
williams <williamskusnandi@gmail.com>
2022-01-11 07:13:08 +1100

    feat: visit demo book button

--- components/DemoBook.tsx
+++ components/DemoBook.tsx
@@ -7,6 +7,9 @@ export const DemoBook = () => {
         className="w-full h-[75vh] border-4 rounded-lg"
         src={process.env.DEMO_URL}
       />
+      <button className="rounded-lg border border-black bg-black text-white text-xl py-4 px-6 mt-2 transition hover:bg-white hover:text-black">
+        Visit
+      </button>
     </div>
   );
 };
